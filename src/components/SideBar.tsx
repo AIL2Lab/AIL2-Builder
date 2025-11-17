bc9eef2cbf3fa0e40ca7d60942d903c109cbb8d7
dio-alexander <dio.alexander000@gmail.com>
2025-11-17 16:58:27 +0800

    fixed menu bug

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,24 +1,23 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 export default function SideBar() {
+    const t = useTranslations("Navbar")
     return (
         <div className="side-bar z-30 px-6 pt-2 fixed flex max-w-full inset-x-0 bottom-0 w-screen flex-col gap-2 overflow-y-auto backdrop-blur-xl backdrop-saturate-150 bg-background/70">
             
             <ul>
                 <li className="py-5">
-                    <Link href="/">Research</Link>
+                    <Link href="/">{t("research")}</Link>
                 </li>
                 <li className="py-5">
-                    <Link href="/">Ecosystem</Link>
+                    <Link href="/">{t("ecosystem")}</Link>
                 </li>
                 <li className="py-5">
-                    <Link href="/">Learn</Link>
+                    <Link href="/">{t("learn")}</Link>
                 </li>
                 <li className="py-5">
-                    <Link href="/">Build</Link>
-                </li>
-                <li className="py-5">
-                    <Link href="/">Blog</Link>
+                    <Link href="/">{t("blog")}</Link>
                 </li>
             </ul>
         </div>
